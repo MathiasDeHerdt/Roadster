e50647710bde561e2a9e0a5e41ce01a1b11a19a9
mathias <mathias.de.herdt@student.howest.be>
2020-11-23 11:10:03 +0100

    backup

--- script/script.js
+++ script/script.js
@@ -1,22 +1,37 @@
 API_URL = "https://api.spacexdata.com/v3/roadster"
 
+const total = 300
 
-async function getRoadsterData(EarthEccentricity){
+const cox = total;
+const coy = total;
+const le = total; 
+
+async function getRoadsterData(){
     const response = await fetch(API_URL);
     const data = await response.json();
     
     const name = data.name;
     const norad = data.norad_id;
     const epoch = data.epoch_jd;
-    const apoapsis = data.apoapsis_au;
-    const periapsis = data.periapsis_au;
+    //const apoapsis = data.apoapsis_au;
+    //const periapsis = data.periapsis_au;
+    const apoapsis = data.periapsis_au;
+    const periapsis = data.apoapsis_au;
+    const periapsis_arg = data.periapsis_arg;
+
     const eccentricity = data.eccentricity;
     const inclination = data.inclination;
-    const longtlongitude = data.longitude;
+    const longitude = data.longitude;
     const days = data.period_days;
     const speed = data.speed_kph;
     const Edistance = data.earth_distance_km;
     const Mdistance = data.mars_distance_km;
+
+    console.log("longitude = " + longitude)
+    console.log("periapsis_arg = "+ periapsis_arg)
+    console.log("inclination = " + inclination)
+    console.log("Edistance = " + Edistance)
+    console.log("Mdistance = " + Mdistance)
 
     Roadster(apoapsis, periapsis, eccentricity)
 }
@@ -33,6 +48,8 @@ const drawXYaxes = function(){
     // Draw the x-axis
     ctx.beginPath();
     ctx.setLineDash([5, 1]);
+    ctx.strokeStyle = "White";
+    // ctx.strokeStyle = "Transparant";
     ctx.moveTo(0, zerocoy);
     ctx.lineTo(zerocox*2, zerocoy);
     ctx.stroke();
@@ -43,10 +60,39 @@ const drawXYaxes = function(){
     ctx.moveTo(zerocox, 0);
     ctx.lineTo(zerocox, zerocoy*2);
     ctx.stroke();
-
 }
 
-const drawPlanetOrbit = function(Planet, Clr, X, MaA, MiA){
+
+const drawPlanet = function(planet, clr, angle, cox, coy, le){
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    const zerocox = cox
+    const zerocoy = coy
+ 
+    //angle = angle + 45
+
+    angle = angle * (-1)
+    console.log("angle = " + angle)
+
+
+    length =  le;
+
+    x1 = zerocox
+    y1 = zerocoy
+
+    x2 = x1 + Math.cos((Math.PI / 180.0) * (angle)) * length
+    y2 = y1 + Math.sin((Math.PI / 180.0) * (angle)) * length
+
+    ctx.beginPath();
+    ctx.setLineDash([5, 5]);
+    ctx.strokeStyle = clr;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+const drawPlanetOrbit = function(Planet, Clr, X, MaA, MiA, cox, coy){
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     
@@ -54,8 +100,8 @@ const drawPlanetOrbit = function(Planet, Clr, X, MaA, MiA){
     console.log(MaA)
     console.log(MiA)
 
-    const zerocox = 300
-    const zerocoy = 300
+    const zerocox = cox
+    const zerocoy = coy
 
     X = zerocox + X
     Y = zerocoy
@@ -67,7 +113,8 @@ const drawPlanetOrbit = function(Planet, Clr, X, MaA, MiA){
     var radiusx = MaA/2;
     var radiusy = MiA/2;
     ctx.beginPath();
-    ctx.ellipse(X, Y, radiusx, radiusy, 0, 0, 360);
+    ctx.ellipse(X, Y, radiusx, radiusy, 0, 0, 2 * Math.PI);
+    
     ctx.stroke();
 }
 
@@ -89,8 +136,8 @@ var Earth = function(){
     const EarthMajorAxisDivided = EarthMajorAxisKM / DivisionFactor
     const EarthMinorAxisDivided = EarthMinorAxisKM / DivisionFactor
 
-    drawPlanetOrbit("Earth","#6483ff", EarthCentralPointXDivided, EarthMajorAxisDivided, EarthMinorAxisDivided)
-
+    drawPlanetOrbit("Earth","Blue", EarthCentralPointXDivided, EarthMajorAxisDivided, EarthMinorAxisDivided, cox, coy)
+    drawPlanet("Earth", "Blue", 61, cox, coy, le)
 }
 
 var Mars = function(){
@@ -111,7 +158,8 @@ var Mars = function(){
     const MarsMajorAxisDivided = MarsMajorAxisKM / DivisionFactor
     const MarsMinorAxisDivided = MarsMinorAxisKM / DivisionFactor
     
-    drawPlanetOrbit("Mars","Red", MarsCentralPointXDivided, MarsMajorAxisDivided, MarsMinorAxisDivided)
+    drawPlanetOrbit("Mars","Red", MarsCentralPointXDivided, MarsMajorAxisDivided, MarsMinorAxisDivided, cox, coy)
+    drawPlanet("Mars", "Red", 44, cox, coy, le)
 }
 
 const Roadster = function(apoapsis, periapsis, eccentricity){
@@ -129,9 +177,10 @@ const Roadster = function(apoapsis, periapsis, eccentricity){
     const MajorAxisDivided = MajorAxisKM / DivisionFactor
     const MinorAxisDivided = MinorAxisKM / DivisionFactor
 
-    drawPlanetOrbit("Roadster", "Violet", CentralPointXDivided, MajorAxisDivided, MinorAxisDivided)
-
+    drawPlanetOrbit("Roadster", "Violet", CentralPointXDivided, MajorAxisDivided, MinorAxisDivided, cox, coy)
+    drawPlanet("Roadster", "violet", 316-270, cox, coy, le)
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOMContentLoaded")
@@ -140,26 +189,3 @@ document.addEventListener('DOMContentLoaded', function() {
     Earth()
     Mars()    
 });
-
-// var venus = function() {
-//     const VenusPeriApsisKM = 107476170
-//     const VenusApoApsisKM = 108942780
-
-//     const DivisionFactor = 1000000
-
-//     const VenusEccentricity = (VenusApoApsisKM-VenusPeriApsisKM)/(VenusApoApsisKM+VenusPeriApsisKM)
-//     const VenusMajorAxisKM = VenusApoApsisKM + VenusPeriApsisKM
-//     const VenusSemiMajorAxisKM = VenusMajorAxisKM / 2
-//     const VenusSemiMinorAxisKM = VenusSemiMajorAxisKM * Math.sqrt(1-(VenusEccentricity*EarthEccentricity))
-//     const VenusMinorAxisKM = VenusSemiMinorAxisKM * 2
-//     const VenusCentralPointKMY = 0
-
-//     const VenusCentralPointKMX = -VenusApoApsisKM + VenusSemiMajorAxisKM
-//     const VenusCentralPointXDivided = VenusCentralPointKMX / DivisionFactor
-//     const VenusMajorAxisDivided = VenusMajorAxisKM / DivisionFactor
-//     const VenusMinorAxisDivided = VenusMinorAxisKM / DivisionFactor
-
-//     drawPlanetOrbit("Venus","Hotpink", VenusCentralPointXDivided, VenusMajorAxisDivided, VenusMinorAxisDivided)
-// }
-
-
